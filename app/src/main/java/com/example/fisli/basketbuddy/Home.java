63985fe11d4f7cdddb6d2831cf78c1fcfcc5cafa
rshellborn <rachel@shellborn.com>
2016-10-07 18:50:51 -0700

    push bitch

---
+++ app/src/main/java/com/example/fisli/basketbuddy/Home.java
@@ -0,0 +1,28 @@
+package com.example.fisli.basketbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    // this is a comment I made on the gitHub text editor
+    // Android Studio: VCS -> Update Project
+    // I can see the above comment in android studio
+    // Android Studio: VCS -> Commit Changes
+    // Success! Now you try (:
+
+
+    //Rachel was here
+    // *whisper* that's a good girl
+
+    //Dalton was here
+    //Trying again
+    // "Cool Beans", said Maks. 
+
+}
---
+++ app/src/main/java/com/example/fisli/basketbuddy/TripSummary.java
@@ -0,0 +1,13 @@
+package com.example.fisli.basketbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_summary);
+    }
+}
