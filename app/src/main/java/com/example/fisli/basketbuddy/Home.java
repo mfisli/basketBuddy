33c53061105504cbfb44e6bf80868a83ef111799
rshellborn <rachel@shellborn.com>
2016-10-07 19:17:00 -0700

    test

--- app/src/main/java/com/example/fisli/basketbuddy/Home.java
+++ app/src/main/java/com/example/fisli/basketbuddy/Home.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Home extends AppCompatActivity {
-//push pl
+//it actually pushed
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
