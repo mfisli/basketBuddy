fcff5cbd82db3e6e0bc37c83ea466ed6d4d832e7
rshellborn <rachel@shellborn.com>
2016-10-14 17:10:27 -0700

    fixing back button on avg spending screen function

--- app/src/main/java/com/example/fisli/basketbuddy/AvgSpending.java
+++ app/src/main/java/com/example/fisli/basketbuddy/AvgSpending.java
@@ -1,7 +1,9 @@
 package com.example.fisli.basketbuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AvgSpending extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class AvgSpending extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_avg_spending);
+    }
+
+    public void backToHome(View v) {
+        Intent myIntent = new Intent(AvgSpending.this, Home.class);
+        AvgSpending.this.startActivity(myIntent);
     }
 }
